268eabc2b1ce3b05367d02748727b155e2ff5d27
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-30 10:40:03 +0530

    Done

--- ContextMenu/ContentView.swift
+++ ContextMenu/ContentView.swift
@@ -10,7 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Image("pic").resizable()
+            .frame(height: 300)
+            .cornerRadius(20)
+            .padding()
+            .contextMenu {
+                
+                VStack {
+                    Button(action: {
+                        print("Save to Gallery")
+                    }) {
+                        
+                        HStack{
+                            Image(systemName: "folder.fill")
+                            Text("Save to Gallery")
+                        }
+                        
+                    }
+                    
+                    Button(action: {
+                        print("Send")
+                    }) {
+                        
+                        HStack{
+                            Image(systemName: "paperplane.fill")
+                            Text("Send")
+                        }
+                        
+                    }
+                    
+                }
+        }
     }
 }
 
